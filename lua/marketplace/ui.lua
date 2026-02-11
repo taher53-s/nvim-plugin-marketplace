4a8a68c1f19ad96196d7c38a08b7ee42db8f0335
Taher Sohagpurwala <tahersohagpurwla@gmail.com>
2026-02-11 19:57:13 +0530

    Style marketplace title and footer text

--- lua/marketplace/ui.lua
+++ lua/marketplace/ui.lua
@@ -38,7 +38,7 @@ function M.open(config)
 		border = config.border,
 	})
 
-	-- close marketplace with q
+	-- Close marketplace with q
 	vim.keymap.set("n", "q", function()
 		if vim.api.nvim_win_is_valid(list_win) then
 			vim.api.nvim_win_close(list_win, true)
@@ -48,15 +48,31 @@ function M.open(config)
 		end
 	end, { buffer = list_buf })
 
-	-- highlight group
+	-----------------------------------------------------------------
+	-- Highlight Groups
+	-----------------------------------------------------------------
 	vim.api.nvim_set_hl(0, "MarketplaceSelected", {
 		bg = "#2a2a2a",
 		bold = true,
 	})
 
-	-- render list and inject preview callback
+	vim.api.nvim_set_hl(0, "MarketplaceTitle", {
+		bold = true,
+	})
+
+	vim.api.nvim_set_hl(0, "MarketplaceFooter", {
+		fg = "#777777",
+	})
+
+	-----------------------------------------------------------------
+	-- Render list
+	-----------------------------------------------------------------
 	buffer.render(list_buf, data.plugins, function(plugin)
-		M.render_preview(preview_buf, plugin)
+		if plugin then
+			M.render_preview(preview_buf, plugin)
+		else
+			vim.api.nvim_buf_set_lines(preview_buf, 0, -1, false, {})
+		end
 	end)
 
 	vim.api.nvim_win_set_cursor(list_win, { 1, 0 })
