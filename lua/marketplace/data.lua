8bf5aa5a9d6e3c26f143cbca0c3859b0bb43c7a1
Taher Sohagpurwala <tahersohagpurwala@gmail.com>
2026-04-07 08:30:48 +0530

    Expand plugin metadata with category field and realistic star counts

--- lua/marketplace/data.lua
+++ lua/marketplace/data.lua
@@ -1,19 +1,34 @@
 local M = {}
 
+-- Available categories for filtering
+M.categories = {
+	"Utility",
+	"Telescope",
+	"File Explorer",
+	"Status Line",
+	"Colorscheme",
+	"LSP",
+	"Completion",
+	"Git",
+}
+
 M.plugins = {
 	{
 		name = "plenary.nvim",
 		desc = "Lua utility functions used by many plugins",
 		repo = "https://github.com/nvim-lua/plenary.nvim",
 		author = "nvim-lua",
-		stars = 0,
+		stars = 2500,
+		category = "Utility",
+		dependencies = {},
 	},
 	{
 		name = "telescope.nvim",
 		desc = "Fuzzy finder",
 		repo = "https://github.com/nvim-telescope/telescope.nvim",
 		author = "nvim-telescope",
-		stars = 0,
+		stars = 18000,
+		category = "Telescope",
 		dependencies = { "plenary.nvim" },
 	},
 	{
@@ -21,14 +36,54 @@ M.plugins = {
 		desc = "File explorer",
 		repo = "https://github.com/nvim-tree/nvim-tree.lua",
 		author = "nvim-tree",
-		stars = 0,
+		stars = 12000,
+		category = "File Explorer",
+		dependencies = {},
 	},
 	{
 		name = "lualine.nvim",
 		desc = "Status line",
 		repo = "https://github.com/nvim-lualine/lualine.nvim",
 		author = "nvim-lualine",
-		stars = 0,
+		stars = 4200,
+		category = "Status Line",
+		dependencies = {},
+	},
+	{
+		name = "catppuccin.nvim",
+		desc = "Soothing pastel theme",
+		repo = "https://github.com/catppuccin/nvim",
+		author = "catppuccin",
+		stars = 8900,
+		category = "Colorscheme",
+		dependencies = {},
+	},
+	{
+		name = "nvim-lspconfig",
+		desc = "LSP configuration",
+		repo = "https://github.com/neovim/nvim-lspconfig",
+		author = "neovim",
+		stars = 15000,
+		category = "LSP",
+		dependencies = {},
+	},
+	{
+		name = "cmp.nvim",
+		desc = "Completion framework",
+		repo = "https://github.com/hrsh7th/cmp.nvim",
+		author = "hrsh7th",
+		stars = 9800,
+		category = "Completion",
+		dependencies = {},
+	},
+	{
+		name = "gitsigns.nvim",
+		desc = "Git integration",
+		repo = "https://github.com/lewis6991/gitsigns.nvim",
+		author = "lewis6991",
+		stars = 6200,
+		category = "Git",
+		dependencies = {},
 	},
 }
 
