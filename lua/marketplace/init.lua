485705022bff1d0c3956dd426a60aef11a82188e
Taher Sohagpurwala <tahersohagpurwala@gmail.com>
2026-04-07 10:48:34 +0530

    Add plugin settings config system with setup function and per-plugin overrides

--- lua/marketplace/init.lua
+++ lua/marketplace/init.lua
@@ -5,15 +5,30 @@ local M = {}
 
 local ui = require("marketplace.ui")
 local state = require("marketplace.state")
+local data = require("marketplace.data")
 
 M.config = {
 	border = "rounded",
 	title = "Plugin Marketplace",
+	-- Default plugin settings per name
+	plugin_settings = {},
 }
+
+-- Merge user-provided plugin settings into plugin_configs
+local function apply_plugin_settings()
+	for name, settings in pairs(M.config.plugin_settings) do
+		if data.plugin_configs[name] then
+			data.plugin_configs[name] = vim.tbl_extend("force", data.plugin_configs[name], settings)
+		end
+	end
+end
 
 -- Allow user configuration (future-proofing)
 function M.setup(opts)
 	M.config = vim.tbl_extend("force", M.config, opts or {})
+
+	-- Apply user plugin settings (e.g. custom priority, lazy flag)
+	apply_plugin_settings()
 
 	state.load()
 	state.load_lockfile()
