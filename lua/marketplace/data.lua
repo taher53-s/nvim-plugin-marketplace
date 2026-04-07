304ff74f7255f42e13cf382f82b42ae9c3f7518a
Taher Sohagpurwala <tahersohagpurwala@gmail.com>
2026-04-07 10:47:31 +0530

    Add dependency field to plugin configs with priority and lazy loading metadata

--- lua/marketplace/data.lua
+++ lua/marketplace/data.lua
@@ -12,79 +12,110 @@ M.categories = {
 	"Git",
 }
 
-M.plugins = {
-	{
-		name = "plenary.nvim",
+-- Plugin configuration (extended metadata + dependency graph)
+M.plugin_configs = {
+	["plenary.nvim"] = {
 		desc = "Lua utility functions used by many plugins",
 		repo = "https://github.com/nvim-lua/plenary.nvim",
 		author = "nvim-lua",
 		stars = 2500,
 		category = "Utility",
-		dependencies = {},
+		dependencies = {},  -- plenary has no deps
+		priority = 50,       -- lower = loaded first
+		lazy = false,        -- load immediately
+		load_event = nil,    -- not lazy
 	},
-	{
-		name = "telescope.nvim",
+	["telescope.nvim"] = {
 		desc = "Fuzzy finder",
 		repo = "https://github.com/nvim-telescope/telescope.nvim",
 		author = "nvim-telescope",
 		stars = 18000,
 		category = "Telescope",
 		dependencies = { "plenary.nvim" },
+		priority = 50,
+		lazy = true,
+		load_event = { event = "VeryLazy" },
 	},
-	{
-		name = "nvim-tree.lua",
+	["nvim-tree.lua"] = {
 		desc = "File explorer",
 		repo = "https://github.com/nvim-tree/nvim-tree.lua",
 		author = "nvim-tree",
 		stars = 12000,
 		category = "File Explorer",
 		dependencies = {},
+		priority = 40,
+		lazy = false,
+		load_event = nil,
 	},
-	{
-		name = "lualine.nvim",
+	["lualine.nvim"] = {
 		desc = "Status line",
 		repo = "https://github.com/nvim-lualine/lualine.nvim",
 		author = "nvim-lualine",
 		stars = 4200,
 		category = "Status Line",
 		dependencies = {},
+		priority = 20,
+		lazy = false,
+		load_event = nil,
 	},
-	{
-		name = "catppuccin.nvim",
+	["catppuccin.nvim"] = {
 		desc = "Soothing pastel theme",
 		repo = "https://github.com/catppuccin/nvim",
 		author = "catppuccin",
 		stars = 8900,
 		category = "Colorscheme",
 		dependencies = {},
+		priority = 10,
+		lazy = false,
+		load_event = nil,
 	},
-	{
-		name = "nvim-lspconfig",
+	["nvim-lspconfig"] = {
 		desc = "LSP configuration",
 		repo = "https://github.com/neovim/nvim-lspconfig",
 		author = "neovim",
 		stars = 15000,
 		category = "LSP",
 		dependencies = {},
+		priority = 30,
+		lazy = true,
+		load_event = { event = "LspAttach" },
 	},
-	{
-		name = "cmp.nvim",
+	["cmp.nvim"] = {
 		desc = "Completion framework",
 		repo = "https://github.com/hrsh7th/cmp.nvim",
 		author = "hrsh7th",
 		stars = 9800,
 		category = "Completion",
 		dependencies = {},
+		priority = 50,
+		lazy = true,
+		load_event = { event = "InsertEnter" },
 	},
-	{
-		name = "gitsigns.nvim",
+	["gitsigns.nvim"] = {
 		desc = "Git integration",
 		repo = "https://github.com/lewis6991/gitsigns.nvim",
 		author = "lewis6991",
 		stars = 6200,
 		category = "Git",
 		dependencies = {},
+		priority = 40,
+		lazy = true,
+		load_event = { event = "VeryLazy" },
 	},
 }
+
+-- Backwards compat: legacy plugin list built from plugin_configs
+M.plugins = {}
+for name, config in pairs(M.plugin_configs) do
+	table.insert(M.plugins, {
+		name = name,
+		desc = config.desc,
+		repo = config.repo,
+		author = config.author,
+		stars = config.stars,
+		category = config.category,
+		dependencies = config.dependencies,
+	})
+end
 
 return M
